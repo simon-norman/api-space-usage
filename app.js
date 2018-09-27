19dc1ac96582afb0b5d4e636d486e58f2a322c0e
Simon Norman <01secn@gmail.com>
2018-09-27 20:34:35 +0100

    Updated main app

--- app.js
+++ app.js
@@ -3,54 +3,17 @@ const { wireUpApp } = require('./dependency_injection/app_wiring');
 const { getConfigForEnvironment } = require('./config/config.js');
 const RavenWrapperFactory = require('raven-wrapper');
 const mongoose = require('mongoose');
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
 let config;
-let diContainer;
 let app;
-
-const configureRouting = () => {
-  app.use(cors());
-
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-
-  const routes = diContainer.getDependency('routes');
-  app.use('/', routes);
-};
-
-const setUpErrorHandler = () => {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-      error: {
-        message: err.message,
-      },
-    });
-  });
-};
-
-const setUpWebServer = () => {
-  app = express();
-
-  configureRouting();
-
-  setUpErrorHandler();
-
-  app.listen(config.webServer.port);
-};
 
 const connectToDatabase = () =>
   mongoose.connect(config.spaceUsageDatabase.uri, { useNewUrlParser: true });
 
 const startApp = async () => {
-  diContainer = wireUpApp();
+  wireUpApp();
 
   config = getConfigForEnvironment(process.env.NODE_ENV);
-
-  setUpWebServer();
 
   await connectToDatabase();
 };
