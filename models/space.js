530e9d08502e4409270f11ced6c807286ea6d26c
Simon Norman <01secn@gmail.com>
2018-09-27 19:21:45 +0100

    Added get spaces and tests

--- models/space.js
+++ models/space.js
@@ -6,7 +6,6 @@ const SpaceSchema = new Schema({
   _id: { type: String, required: true },
   name: { type: String, required: true },
   occupancyCapacity: { type: Number, required: true },
-  siteId: { type: String, required: true },
 });
 
 const Space = mongoose.model('Space', SpaceSchema);
--- routes/space_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-module.exports = (spaceController) => {
-  const router = express.Router();
-
-  router.get('/', spaceController.getSpaces);
-
-  return router;
-};
